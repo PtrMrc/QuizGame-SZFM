cd177fb06194c4a3b119a757818d155a877c5443
Fekete Domán Csaba <doman0902@gmail.com>
2025-09-24 21:11:56 +0200

    app.py létrehozva

---
+++ quiz-app/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__, static_folder="static", template_folder="templates")
+
+
+# kezdőlap (pl. main.html – ezt neked kell hozzáadni, ahol névbevitel van)
+@app.route("/")
+def index():
+    return render_template("main.html")  # majd csinálj egy main.html fájlt
+
+
+# kérdések oldal
+@app.route("/quiz")
+def quiz():
+    return render_template("quiz.html")
+
+
+# végeredmény oldal
+@app.route("/end")
+def end():
+    return render_template("end.html")
+
+
+# kérdések JSON kiszolgálása
+@app.route("/questions.json")
+def questions():
+    return send_from_directory("static", "questions.json")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
