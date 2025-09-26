fa160c4b969a7cf3bfcc4cb98d3a40fbbffde951
Péter Marcell <pmarcell.1021@gmail.com>
2025-09-26 12:37:25 +0200

    Kommentek eltávolítása

--- quiz-app/app/__init__.py
+++ quiz-app/app/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 import os
 
-# optional: absolute paths to avoid template/static issues
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 app = Flask(
@@ -10,5 +9,4 @@ app = Flask(
     template_folder=os.path.join(BASE_DIR, "app", "templates")
 )
 
-# import routes at the end to avoid circular imports
 from app import routes
